081f8c7b2f6c154fea2f45297eb6728ac43a4362
MachsunSSR <safirmachsun@gmail.com>
2022-11-23 14:12:06 +0700

    Update requirenments.txt

--- app.py
+++ app.py
@@ -1,32 +1,28 @@
-from gettext import find
-from http.client import responses
-import os
-from unittest import result
 from flask import Flask, request, jsonify
 import numpy as np
 import pandas as pd
 import pickle
 import re
 
-app = Flask(__name__)
+app = Flask(_name_)
 app.config['JSON_SORT_KEYS'] = False
 
 model = pickle.load(open("model.pkl", "rb"))
 
 
-@app.route('/predict2', methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict1():
     if request.method == 'POST':
-        item1 = int(request.form['input1'])
-        item2 = int(request.form['input2'])
-        item3 = int(request.form['input3'])
-        item4 = int(request.form['input4'])
-        item5 = int(request.form['input5'])
-        item6 = int(request.form['input6'])
-        item7 = int(request.form['input7'])
-        item8 = int(request.form['input8'])
-        item9 = int(request.form['input9'])
-        item10 = int(request.form['input10'])
+        item1 = int(request.form.get('item1'))
+        item2 = int(request.form.get('item2'))
+        item3 = int(request.form.get('item3'))
+        item4 = int(request.form.get('item4'))
+        item5 = int(request.form.get('item5'))
+        item6 = int(request.form.get('item6'))
+        item7 = int(request.form.get('item7'))
+        item8 = int(request.form.get('item8'))
+        item9 = int(request.form.get('item9'))
+        item10 = int(request.form.get('item10'))
         item = {
             'Tidak mau makan': [item1],
             'Luka/lepuh pada daerah mulut': [item2],
@@ -37,16 +33,16 @@ def predict1():
             'hewan lebih senang berbaring': [item7],
             'Mengalami salah satu gejala Menggeretakkan gigi/ menggosokkan mulut/ leleran mulut': [item8],
             'terjadi penurunan produksi susu': [item9],
-            'nafas cepat': [item10],
+            'nafas cepat': [item10]
         }
         data = pd.DataFrame(item)
         y_pred = model.predict(data)
+        fix = y_pred[0]
+        datafinal = int(fix)
         return jsonify(
             {
                 "status": "Success",
                 "message": "Successfully making prediction",
-                "data": {
-                    "input1": y_pred,
-                }
+                "data": datafinal
             }
         )
